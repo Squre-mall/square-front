27c407623bbbdb500efc8e5e536b13c98ae6f68a
l2s <48622670+leehosu@users.noreply.github.com>
2020-02-18 22:59:29 +0900

    build header, menu, contents useing marterial-ui

--- squre/src/Component/Contents/Contents.tsx
+++ squre/src/Component/Contents/Contents.tsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles({
+  contents: {
+    paddingLeft: 500
+    // backgroundColor: "yellow"
+  },
+  itemBox: {
+    display: "flex",
+    flexWrap: "wrap",
+    width: 1000
+  },
+  item: {
+    padding: 30
+  },
+  paper: {
+    width: 400,
+    height: 400
+  }
+});
 
 const Contents = () => {
-  return <div>contents</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.contents}>
+      <Typography variant="h3"> 전체 목록 </Typography>
+      <Box className={classes.itemBox}>
+        <Box className={classes.item}>
+          <Paper square={false} elevation={2} className={classes.paper}>
+            list1
+          </Paper>
+        </Box>
+        <Box className={classes.item}>
+          <Paper square={false} elevation={2} className={classes.paper}>
+            list2
+          </Paper>
+        </Box>
+        <Box className={classes.item}>
+          <Paper square={false} elevation={2} className={classes.paper}>
+            list3
+          </Paper>
+        </Box>
+        <Box className={classes.item}>
+          <Paper square={false} elevation={2} className={classes.paper}>
+            list4
+          </Paper>
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default Contents;
--- squre/src/Component/Header/Header.tsx
+++ squre/src/Component/Header/Header.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles({
+  header: {
+    width: "100%",
+    height: 100,
+    display: "flex"
+  },
+  titleBox: {
+    flex: 1
+  },
+  title: {
+    padding: 30,
+    fontSize: 30,
+    fontFamily: "Raleway",
+    fontStyle: "normal",
+    fontDisplay: "swap",
+    fontWeight: 400
+  },
+  buttonBox: {
+    padding: 30
+  }
+});
 
 const Header = () => {
-
+  const classes = useStyles();
   return (
-    <div>
-      header
+    <div className={classes.header}>
+      <Box className={classes.titleBox}>
+        <Typography className={classes.title} variant="h3">
+          Squre
+        </Typography>
+      </Box>
+      <Box className={classes.buttonBox}>
+        <Button variant="contained" color="primary" disableElevation>
+          Log In
+        </Button>
+      </Box>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
