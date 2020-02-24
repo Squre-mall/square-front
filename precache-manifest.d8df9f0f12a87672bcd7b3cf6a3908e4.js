59890ef9183360cc0044f8c2fbf7143cfb2fe2ba
l2s <48622670+leehosu@users.noreply.github.com>
2020-02-24 20:23:51 +0900

    Updates

---
+++ precache-manifest.d8df9f0f12a87672bcd7b3cf6a3908e4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53081aa3c4d02e7ee4eb53598cec9ef8",
+    "url": "/square/index.html"
+  },
+  {
+    "revision": "903e997b30d75d979fb9",
+    "url": "/square/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a4c9b7716f60aa01ac3a",
+    "url": "/square/static/js/2.9209d6f2.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/square/static/js/2.9209d6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "903e997b30d75d979fb9",
+    "url": "/square/static/js/main.b0112cf9.chunk.js"
+  },
+  {
+    "revision": "b8b9f487a58e43d0bb78",
+    "url": "/square/static/js/runtime-main.a2146862.js"
+  },
+  {
+    "revision": "af4761c9375c571169947db9f475eb24",
+    "url": "/square/static/media/bottom-1.af4761c9.jpg"
+  },
+  {
+    "revision": "2a1da94d6fa4b3d74fa37731b3cfeb60",
+    "url": "/square/static/media/bottom-2.2a1da94d.jpg"
+  },
+  {
+    "revision": "f9fc5775e97e686a67cbadd470f2f4e8",
+    "url": "/square/static/media/outer-1.f9fc5775.png"
+  },
+  {
+    "revision": "f3fdc687d66aa02a46df56ad772ca67f",
+    "url": "/square/static/media/square-icon.f3fdc687.png"
+  },
+  {
+    "revision": "3f050ead07bcaa13c150331d5c2737f8",
+    "url": "/square/static/media/top-1.3f050ead.jpg"
+  },
+  {
+    "revision": "34145c5a224d5b77406de115ad28d7d4",
+    "url": "/square/static/media/top-3.34145c5a.jpg"
+  }
+]);
