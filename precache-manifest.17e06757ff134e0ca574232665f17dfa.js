92a25829b7243a8c0244c2c093d08344dc8caf64
l2s <48622670+leehosu@users.noreply.github.com>
2020-02-26 10:42:51 +0900

    Updates

--- precache-manifest.17e06757ff134e0ca574232665f17dfa.js
+++ precache-manifest.17e06757ff134e0ca574232665f17dfa.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e27be4943703808c3c806d5fcc5d3ca3",
+    "revision": "4ccc0dcee3f37303c520019efe725ed0",
     "url": "/square-front/index.html"
   },
   {
-    "revision": "6687cca430bc37956489",
+    "revision": "ba02885b75d65b546484",
     "url": "/square-front/static/css/main.69eda509.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/square-front/static/js/2.2a43fb98.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6687cca430bc37956489",
-    "url": "/square-front/static/js/main.3c31bd3b.chunk.js"
+    "revision": "ba02885b75d65b546484",
+    "url": "/square-front/static/js/main.7d2d7853.chunk.js"
   },
   {
     "revision": "3d384ffc9dc8ee1dbeb0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/square-front/precache-manifest.bbfd9d233df249cf1e82c74a2024e47c.js"
+  "/square-front/precache-manifest.17e06757ff134e0ca574232665f17dfa.js"
 );
 
 self.addEventListener('message', (event) => {
