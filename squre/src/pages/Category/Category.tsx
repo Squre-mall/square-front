d50059fbd65b12fb44cc21dec05d890621753a72
l2s <48622670+leehosu@users.noreply.github.com>
2020-03-16 22:11:30 +0900

    refactoring about types

--- squre/src/pages/Category/Category.tsx
+++ squre/src/pages/Category/Category.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { SFC } from "react";
+import { RouteComponentProps } from "react-router";
 import CategoryContainer from "../../Container/CategoryContainer";
 
-const Category = ({ match }) => {
+interface MatchParams {
+  category: string;
+}
+
+const Category: SFC<RouteComponentProps<MatchParams>> = ({ match }) => {
   return (
     <div className="category">
       <CategoryContainer category={match.params.category} />
